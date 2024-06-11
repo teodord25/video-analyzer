4d69490ee25e4afd250b0e779b5f1b796f61c856
tedord25 <djuric.teodor25@gmail.com>
2024-06-11 11:00:53 +0200

    click me

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,22 @@ use leptos::*;
 fn main() {
     console_error_panic_hook::set_once();
 
-    mount_to_body(|| view! { <p>{"Hello, World!"}</p> })
+    mount_to_body(|| view! { <App/> })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1)
+            }
+        >
+            "Click me: "
+            // on stable, this is move || count.get();
+            {count}
+        </button>
+    }
 }
