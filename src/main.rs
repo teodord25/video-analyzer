c59d7fe82a291119a9daaf7538409e2651a5fd4c
tedord25 <djuric.teodor25@gmail.com>
2024-06-11 23:15:25 +0200

    click to move dynamic styles

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,22 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
+    let (x, set_x) = create_signal(0);
 
     view! {
         <button
             on:click=move |_| {
-                set_count.update(|n| *n += 1)
+                set_x.update(|n| *n += 10)
             }
-        >
-            "Click me: "
-            // on stable, this is move || count.get();
-            {count}
+
+            style="position: absolute"
+
+            style:left=move || format!("{}px", x() + 100)
+            style:background-color=move || format!("rgb({}, {}, 100)", x(), 100)
+            style:max-width="400px"
+            style=("--columns", x)
+                >
+            "Click to move"
         </button>
     }
 }
