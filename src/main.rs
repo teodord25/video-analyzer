fc8cb1aacf8dfa29b87a9bfee0e51a4e5a141a2a
tedord25 <djuric.teodor25@gmail.com>
2024-06-13 12:25:45 +0200

    impl progress as separate component

--- src/main.rs
+++ src/main.rs
@@ -9,49 +9,23 @@ fn main() {
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
-
-    let (x, set_x) = create_signal(0);
-
-    let double_count = move || count() * 2;
-
-    let html = "<p> This html will be injected </p>";
-
     view! {
-        <button
-            on:click=move |_| {
-                set_count.update(|n| *n += 1)
-            }
-
-            class:red=move || count() % 2 == 1
-        >
-            "Click me: "
-            // on stable, this is move || count.get();
-            {count}
+        <button on:click=move |_| set_count(count.get() + 1)>
+            Click me
         </button>
 
-        <button
-            on:click=move |_| {
-                set_x.update(|n| *n += 10)
-            }
+        <ProgressBar progress=count/>
+    }
+}
 
-            style="position: absolute"
-
-            style:left=move || format!("{}px", x() + 100)
-            style:background-color=move || format!("rgb({}, {}, 100)", x(), 100)
-            style:max-width="400px"
-            style=("--columns", x)
-                >
-            "Click to move"
-        </button>
-
+#[component]
+fn ProgressBar(
+    progress: ReadSignal<i32>
+) -> impl IntoView {
+    view! {
         <progress
-                max="50"
-                value=double_count
+            max="50"
+            value=progress
         />
-
-        <p inner_html=html>
-            "Double count: "
-            {double_count}
-        </p>
     }
 }
