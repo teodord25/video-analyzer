663bbf7c86138915e2788a898da5b990fe11aaeb
tedord25 <djuric.teodor25@gmail.com>
2024-06-13 16:47:17 +0200

    generic signal prop thing

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,66 @@ fn main() {
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
+    let double_count = move || count() * 2;
+
     view! {
-        <button on:click=move |_| set_count(count.get() + 1)>
+        <button on:click=move |_| { set_count.update(|n| *n += 1); }>
             Click me
         </button>
 
         <ProgressBar progress=count/>
+        <ProgressBar progress=double_count/>
     }
 }
 
 #[component]
 fn ProgressBar(
-    progress: ReadSignal<i32> // this is a `property`, which is like an HTML attribute
+    #[prop(optional)] // optional means it can be omitted from the tag (but then the default will
+    // be Default::default() for the type)
+
+    #[prop(default = 10)] // you can set a default value for a prop
+    max: u16,
+
+    progress: impl Fn() -> i32 + 'static
 ) -> impl IntoView {
     view! {
         <progress
-            max="50"
+            max=max
             value=progress
         />
     }
 }
+
+// NOTE: you can use where clause
+// #[component]
+// fn ProgressBar2<F>(
+//     #[prop(default = 0)]
+//     max: u16,
+//     progress: F
+// ) -> impl IntoView
+// where
+//     F: Fn() -> i32 + 'static
+// {
+//     view! {
+//         <progress
+//             max=max
+//             value=progress
+//         />
+//     }
+// }
+
+
+// NOTE: you can also inline generic
+// #[component]
+// fn ProgressBar3<F: Fn() -> i32 + 'static>(
+//     #[prop(default = 0)]
+//     max: u16,
+//     progress: F
+// ) -> impl IntoView {
+//     view! {
+//         <progress
+//             max=max
+//             value=progress
+//         />
+//     }
+// }
