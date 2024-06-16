5db3f414af7fbef383529439db33caf795065c75
tedord25 <djuric.teodor25@gmail.com>
2024-06-16 12:25:57 +0200

    child -> parent communication simple

--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,30 @@ use leptos::*;
 fn main() {
     console_error_panic_hook::set_once();
 
-    mount_to_body(|| view! { <NumericInput/> })
+    mount_to_body(|| view! { <App/> })
+}
+
+// child -> parent communication
+//
+//
+// 1. passing a write signal
+// NOTE: simple but not recommended
+#[component]
+pub fn App() -> impl IntoView {
+    let (toggled, set_toggled) = create_signal(false);
+    view! {
+        <p>"Toggled? " {toggled}</p>
+        <ButtonA setter=set_toggled/> // here the ability to mutate the signal is passed to the child
+    }
 }
 
 #[component]
-fn NumericInput() -> impl IntoView {
-    let (value, set_value) = create_signal(Ok(0));
-
-    let on_input = move |ev| set_value(event_target_value(&ev).parse::<i32>());
-
+pub fn ButtonA(setter: WriteSignal<bool>) -> impl IntoView {
     view! {
-        <h1>"Error Handling"</h1>
-        <label>
-            "Type a number (or something thats not)"
-            <input type="number" on:input=on_input />
-
-
-            // NOTE: we use this error boundary thing because Ok(T) is rendered
-            // but Err(E) is just not rendered, so to show the error we use this
-            <ErrorBoundary
-                // the fallback receives a signal containing current errors
-                fallback=|errors| view! {
-                    <div class="error">
-                        <p>"Thats not a number"</p>
-                        // we can render a list of errors as strings if we want
-                        <ul>
-                            { move || errors.get()
-                                    .into_iter()
-                                    .map(|(_, e)| view! { <li>{e.to_string()}</li> })
-                                    .collect_view()
-                            }
-                        </ul>
-                    </div>
-                }
-            >
-                <p>"You entered: "{value}</p>
-            </ErrorBoundary>
-        </label>
+        <button
+            on:click=move |_| setter.update(|value| *value = !*value)
+        >
+            "Toggle"
+        </button>
     }
 }
