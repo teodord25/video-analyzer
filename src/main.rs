89061a51fe1f7ca55412b5d166873272c346f68f
tedord25 <djuric.teodor25@gmail.com>
2024-06-16 12:32:22 +0200

    passing a callback to the child

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use leptos::*;
+use leptos::ev::MouseEvent;
 
 fn main() {
     console_error_panic_hook::set_once();
@@ -8,24 +9,24 @@ fn main() {
 
 // child -> parent communication
 //
-//
-// 1. passing a write signal
-// NOTE: simple but not recommended
+// 2. passing a callback from parent to child
 #[component]
 pub fn App() -> impl IntoView {
     let (toggled, set_toggled) = create_signal(false);
     view! {
         <p>"Toggled? " {toggled}</p>
-        <ButtonA setter=set_toggled/> // here the ability to mutate the signal is passed to the child
+        // you pass the callback to the child component
+        <ButtonB on_click=move |_| set_toggled.update(|value| *value = !*value)/>
     }
 }
 
+
 #[component]
-pub fn ButtonA(setter: WriteSignal<bool>) -> impl IntoView {
+pub fn ButtonB(#[prop(into)] on_click: Callback<MouseEvent>) -> impl IntoView
+{
     view! {
-        <button
-            on:click=move |_| setter.update(|value| *value = !*value)
-        >
+        <button on:click=on_click> // the child click event is only used to execute the on_click
+        // function that was passed
             "Toggle"
         </button>
     }
