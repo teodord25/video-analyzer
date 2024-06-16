bcc7b4a810fa96bf6d311660f3599fa111a01e6b
tedord25 <djuric.teodor25@gmail.com>
2024-06-16 12:41:11 +0200

    context api

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,59 @@ fn main() {
 
 // child -> parent communication
 //
-// 3. using an event listener if the callback maps to a native dom element
+// 4. Context API
 #[component]
 pub fn App() -> impl IntoView {
     let (toggled, set_toggled) = create_signal(false);
+
+    // share `set_toggled` with all children of this component
+    provide_context(set_toggled);
+
     view! {
         <p>"Toggled? " {toggled}</p>
-        <ButtonB on:click=move |_| set_toggled.update(|value| *value = !*value)/>
-        // using on:click now (event listener) instead of on_click (callback arg)
+        <Layout/>
     }
 }
+
+// <Layout/> and <Content/> omitted
+// To work in this version, drop their references to set_toggled
 
 
 #[component]
-pub fn ButtonB() -> impl IntoView {
+pub fn Layout() -> impl IntoView {
     view! {
-        <button> "Toggle" </button>
+        <header>
+            <h1>"My Page"</h1>
+        </header>
+        <main>
+            <Content/>
+        </main>
     }
 }
+
+#[component]
+pub fn Content() -> impl IntoView {
+    view! {
+        <div class="content">
+            <ButtonD/>
+        </div>
+    }
+}
+
+#[component]
+pub fn ButtonD() -> impl IntoView {
+    // use_context searches up the context tree, hoping to
+    // find a `WriteSignal<bool>`
+    // in this case, I .expect() because I know I provided it
+    let setter = use_context::<WriteSignal<bool>>()
+        .expect("to have found the setter provided");
+
+    view! {
+        <button
+            on:click=move |_| setter.update(|value| *value = !*value)
+        >
+            "Toggle"
+        </button>
+    }
+}
+
